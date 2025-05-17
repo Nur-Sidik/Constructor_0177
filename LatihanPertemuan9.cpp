6c54b827dd2d7866d5b59c510feb502496244b5e
Nur-Sidik <nursidik1500@gmail.com>
2025-05-17 21:08:34 +0700

    membuat class privte dan public pada mahasiswa

--- LatihanPertemuan9.cpp
+++ LatihanPertemuan9.cpp
@@ -6,3 +6,13 @@ class Mahasiswa;
 class Dosen;
 class Staff;
 class Universitas;
+
+class Mahasiswa
+{
+    private:
+        float Nilai;
+        friend class Dosen;
+    public:
+        string Nama;
+        string Nim;
+};
